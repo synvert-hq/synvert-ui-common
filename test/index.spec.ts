f8bae098a591fe95b2d180bc7b929e840273ab72
Richard Huang <flyerhzm@gmail.com>
2023-01-14 12:39:00 +0800

    test filterSnippets and sortSnippets

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,51 @@
 import dedent from "dedent";
-import { composeRubyGeneratedSnippet, composeJavascriptGeneratedSnippet } from "../src/index";
+import { filterSnippets, sortSnippets, composeRubyGeneratedSnippet, composeJavascriptGeneratedSnippet } from "../src/index";
+
+describe("filterSnippets", () => {
+  it("gets snippets when text exists in group", () => {
+    const snippets = [
+      { id: 1, group: "group1", name: "name1", description: "description1", source_code: "" },
+      { id: 2, group: "group2", name: "name2", description: "description2", source_code: "" },
+    ];
+    const text = "group1";
+    expect(filterSnippets(snippets, text)).toEqual([snippets[0]]);
+  });
+
+  it("gets snippets when text exists in name", () => {
+    const snippets = [
+      { id: 1, group: "group1", name: "name1", description: "description1", source_code: "" },
+      { id: 2, group: "group2", name: "name2", description: "description2", source_code: "" },
+    ];
+    const text = "name1";
+    expect(filterSnippets(snippets, text)).toEqual([snippets[0]]);
+  });
+
+  it("gets snippets when text exists in description", () => {
+    const snippets = [
+      { id: 1, group: "group1", name: "name1", description: "description1", source_code: "" },
+      { id: 2, group: "group2", name: "name2", description: "description2", source_code: "" },
+    ];
+    const text = "description1";
+    expect(filterSnippets(snippets, text)).toEqual([snippets[0]]);
+  });
+});
+
+describe("sortSnippets", () => {
+  it("sorts snippets alphabetically, if text matches in group, sort first, if text matches in name, sort second", () => {
+    const snippets = [
+      { id: 1, group: "group ruby", name: "name rspec", description: "description rspec", source_code: "" },
+      { id: 2, group: "group rails", name: "name rspec", description: "description ruby", source_code: "" },
+      { id: 3, group: "group rspec", name: "name ruby", description: "description rails", source_code: "" },
+      { id: 4, group: "group ruby", name: "name rails", description: "description rails", source_code: "" },
+    ];
+    const text = "ruby";
+    sortSnippets(snippets, text);
+    expect(snippets[0].id).toEqual(4);
+    expect(snippets[1].id).toEqual(1);
+    expect(snippets[2].id).toEqual(3);
+    expect(snippets[3].id).toEqual(2);
+  })
+});
 
 describe("composeRubyGeneratedSnippet", () => {
   it("compose snippet for ruby", () => {
