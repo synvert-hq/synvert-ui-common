e906be20a2670d711937907ca4a06c38851618e8
Richard Huang <flyerhzm@gmail.com>
2023-02-04 14:08:26 +0800

    Revert "add runShellCommand function"
    This reverts commit 4945c14b21acaa42f5a3c3cc2a2afb0d3cd1c010.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,8 +4,7 @@ import {
   sortSnippets,
   parseJSON,
   composeRubyGeneratedSnippet,
-  composeJavascriptGeneratedSnippet,
-  runShellCommand,
+  composeJavascriptGeneratedSnippet
 } from "../src/index";
 
 describe("filterSnippets", () => {
@@ -110,19 +109,5 @@ describe("composeJavascriptGeneratedSnippet", () => {
       });
     `;
     expect(composeJavascriptGeneratedSnippet({ filePattern, nodeVersion, npmVersion, snippet })).toEqual(composedSnippet);
-  });
-});
-
-describe("runShellCommand", () => {
-  it("runs shell command correctly", async () => {
-    const result = await runShellCommand("echo", ["hello world"]);
-    expect(result.stdout).toEqual("hello world\n");
-    expect(result.stderr).toEqual("");
-  });
-
-  it("runs shell command incorrectly", async () => {
-    const result = await runShellCommand("ohce", ["hello world"]);
-    expect(result.stdout).toEqual("");
-    expect(result.stderr).toContain("command not found: ohce");
   });
 });
