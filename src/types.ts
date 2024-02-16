b92aa7dbd4b45fd2bc6a57951336dcbe34f0ec53
Richard Huang <flyerhzm@gmail.com>
2024-02-16 22:01:04 +0800

    nqlOrRules is a string "rules" or "nql"

--- src/types.ts
+++ src/types.ts
@@ -39,7 +39,7 @@ export type GenerateSnippetsParams = {
   gemVersion: string,
   inputs: string[],
   outputs: string[],
-  nqlOrRules: boolean,
+  nqlOrRules: "nql" | "rules",
 } | {
   language: "javascript" | "typescript" | "css" | "less" | "sass" | "scss",
   parser: string,
@@ -48,7 +48,7 @@ export type GenerateSnippetsParams = {
   npmVersion: string,
   inputs: string[],
   outputs: string[],
-  nqlOrRules: boolean,
+  nqlOrRules: "nql" | "rules",
 }
 
 export type RunCommandType = (command: string, args: string[], options?: { input?: string }) => Promise<{ output: string, error?: string }>;
