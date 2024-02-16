4fa1c4e68db9a5c57c393048d72728dcf01bf274
Richard Huang <flyerhzm@gmail.com>
2024-02-16 22:01:57 +0800

    language is type LANGUAGE

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { PARSERS, FILE_PATTERNS, PLACEHOLDERS } from './constant';
+import { LANGUAGE } from './types';
 
 export { formatCommandResult, handleTestResults, runSynvertRuby, runSynvertJavascript, checkRubyDependencies, checkJavascriptDependencies, DependencyResponse } from './command';
 export { filterSnippets, sortSnippets, fetchSnippets, generateSnippets } from './snippet';
@@ -7,27 +8,27 @@ export type { Snippet } from "./types";
 
 /**
  * Get file pattern by language.
- * @param {string} language
+ * @param {LANGUAGE} language
  * @returns {string}
  */
-export function filePatternByLanguage(language: string): string {
+export function filePatternByLanguage(language: LANGUAGE): string {
   return FILE_PATTERNS[language];
 }
 
 /**
  * Get placeholder by language.
- * @param {string} language
+ * @param {LANGUAGE} language
  * @returns {string}
  */
-export function placeholderByLanguage(language: string): { [name: string]: string } {
+export function placeholderByLanguage(language: LANGUAGE): { [name: string]: string } {
   return PLACEHOLDERS[language];
 }
 
 /**
  * Get parsers by language.
- * @param {string} language
+ * @param {LANGUAGE} language
  * @returns {string[]}
  */
-export function parsersByLanguage(language: string): string[] {
+export function parsersByLanguage(language: LANGUAGE): string[] {
   return PARSERS[language];
 }
